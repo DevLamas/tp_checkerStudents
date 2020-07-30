64a811bab84c3a90ad882fd333a5495f848d4dc1
DevLamas <quentin.doppler@viacesi.fr>
2020-07-30 14:44:08 +0200

    Commit Modif Classe Attribut dateN (QD)

--- src/main/java/model/Eleve.java
+++ src/main/java/model/Eleve.java
@@ -1,13 +1,81 @@
 package model;
 
+import java.sql.Date;
+
 public class Eleve {
 	private int id;
 	private String nom;
 	private String prenom;
-	private Date dateN;
+	private String dateN;
 	private Classe classe;
 	private String login;
 	private String password;
+	
+	public Eleve(int id, String nom, String prenom, String dateN, Classe classe, String login, String password) {
+		this.id = id;
+		this.nom = nom;
+		this.prenom = prenom;
+		this.dateN = dateN;
+		this.classe = classe;
+		this.login = login;
+		this.password = password;
+	}
+
+	public String getNom() {
+		return nom;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+	public String getPrenom() {
+		return prenom;
+	}
+
+	public void setPrenom(String prenom) {
+		this.prenom = prenom;
+	}
+
+	public String getDateN() {
+		return dateN;
+	}
+
+	public void setDateN(String dateN) {
+		this.dateN = dateN;
+	}
+
+	public Classe getClasse() {
+		return classe;
+	}
+
+	public void setClasse(Classe classe) {
+		this.classe = classe;
+	}
+
+	public String getLogin() {
+		return login;
+	}
+
+	public void setLogin(String login) {
+		this.login = login;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public int getId() {
+		return id;
+	}
+	
+	
+	
+	
 	
 	
 }
