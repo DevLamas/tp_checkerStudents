5f93c56dfbb3d6a9edde2dcb6273a5f73c212177
DevLamas <quentin.doppler@viacesi.fr>
2020-07-30 15:04:53 +0200

    Function connexion() modifier

--- src/main/java/model/ConnexionBDD.java
+++ src/main/java/model/ConnexionBDD.java
@@ -13,20 +13,54 @@ import java.util.Properties;
 public class ConnexionBDD {
 	private Connection bdd;
 	
-	public ConnexionBDD() {
+	/*
+	public static Connection connexion() {
 		try {
 			Properties prop = new Properties();
 			try {
 				InputStream input = new FileInputStream("src/main/ressources/config.properties");
 				prop.load(input);
+				
+				String url = prop.getProperty("db.url");
+	            String user = prop.getProperty("db.user");
+	            String passwd = prop.getProperty("db.password");
+
+	            Connection con = DriverManager.getConnection(url,user,passwd);
+	            return con;
+	            
 			} catch (final IOException ex) {
 				ex.printStackTrace();
 			}
-        	this.bdd = DriverManager.getConnection(prop.getProperty("db.url"),prop.getProperty("db.username "),prop.getProperty("db.password "));
         } catch (SQLException ex) {
         	System.out.println("SQLException: " + ex.getMessage());
             System.out.println("SQLState: " + ex.getSQLState());
             System.out.println("VendorError: " + ex.getErrorCode());
+            return null;
         }
+		return null;
 	}
+	*/
+	
+	public static Connection connexion() throws ClassNotFoundException, SQLException {
+        final Properties prop = new Properties();
+        InputStream input = null;
+        try {
+            input = new FileInputStream("src/main/resources/config.properties");
+            prop.load(input);
+
+            //Class.forName(prop.getProperty("db.driver"));
+            String url = prop.getProperty("db.url");
+            String user = prop.getProperty("db.user");
+            String passwd = prop.getProperty("db.password");
+
+            Connection con = DriverManager.getConnection(url,user,passwd);
+            return con;
+        }catch(Exception e){
+            System.out.println("Une erreure est survenue lors de la connexion de la base de données : ");
+        	System.out.println("SQLException: " + e.getMessage());
+            e.printStackTrace();
+            return null;
+        }
+    }
+	
 }
